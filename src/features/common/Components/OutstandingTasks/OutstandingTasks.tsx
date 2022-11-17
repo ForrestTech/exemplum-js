8fa3f6b29405727ffd74247cf9039c99b4b9f4b3
Richard Forrest <richard.a.forrest@gmail.com>
2022-11-17 13:33:30 +0900

    Vercel setting update

--- src/features/common/Components/OutstandingTasks/OutstandingTasks.tsx
+++ src/features/common/Components/OutstandingTasks/OutstandingTasks.tsx
@@ -2,7 +2,6 @@ export const OutstandingTasks = () => (
   <div>
     <p className="font-medium dark:text-white">Still to do:</p>
     <ul className="dark:text-white">
-      <li>Check DB setting verecel</li>
       <li>Test migrating to toastify</li>
       <li>
         Set priority level feature: enum helpers for typescript (priority level)
